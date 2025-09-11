72ee2665e5b3a3c5a09e34bec245d579d78c46ac
Zucchini10 <ryan.lim.2024@computing.smu.edu.sg>
2025-09-11 15:30:04 +0800

    fkn hell

--- tariff-app/next.config.ts
+++ tariff-app/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",   // tells Next.js to generate stat
+  
 };
 
 export default nextConfig;
