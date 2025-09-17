4e784cef162b9548fedcaf0f7eeae654cf7e837f
BradSquare <gohbradley@gmail.com>
2025-09-18 02:10:24 +0800

    Fix Linting Error

--- tariff-app/src/components/ui/globe.tsx
+++ tariff-app/src/components/ui/globe.tsx
@@ -30,6 +30,16 @@ type Position = {
   color: string;
 };
 
+type Arc = {
+  startLat: number;
+  startLng: number;
+  endLat: number;
+  endLng: number;
+  color: string;
+  arcAlt: number;
+  order: number;
+};
+
 export type GlobeConfig = {
   pointSize?: number;
   globeColor?: string;
@@ -163,7 +173,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
       .arcStartLng((d) => (d as { startLng: number }).startLng * 1)
       .arcEndLat((d) => (d as { endLat: number }).endLat * 1)
       .arcEndLng((d) => (d as { endLng: number }).endLng * 1)
-      .arcColor((e) => (e as { color: string }).color)
+      .arcColor('color')
       .arcAltitude((e) => (e as { arcAlt: number }).arcAlt * 1)
       .arcStroke(() => [0.32, 0.28, 0.3][Math.round(Math.random() * 2)])
       .arcDashLength(defaultProps.arcLength)
