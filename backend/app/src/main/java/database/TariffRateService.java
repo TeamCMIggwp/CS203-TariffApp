3515f26123061bc342f3d54135a7c90c7c97d6e7
Zucchini10 <ryan.lim.2024@computing.smu.edu.sg>
2025-09-29 21:02:45 +0800

    Revert "Merge branch 'main' of https://github.com/TeamCMIggwp/CS203-TariffApp"
    This reverts commit 201e3ca1bbd934e845bd9d4dc519c584e5ee8576, reversing
    changes made to 4c4d9bd938a5f64909eec9ba143e1a286389298a.

---
+++ backend/app/src/main/java/database/TariffRateService.java
@@ -0,0 +1,19 @@
+package database;
+
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TariffRateService {
+    private final TariffRateRepository repo;
+
+    public TariffRateService(TariffRateRepository repo) {
+        this.repo = repo;
+    }
+
+    /** Returns DB rate as text (so WitsApiService can return a String body easily). */
+    public Optional<String> retrieveTariffRateAsText(String reporter, String partner, Integer product, String year) {
+        Double rate = repo.getTariffRate(reporter, partner, product, year); // all Strings
+        return Optional.ofNullable(rate).map(String::valueOf);
+    }
+}
