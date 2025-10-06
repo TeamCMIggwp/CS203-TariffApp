23f950948b5c1fc49a74f5f7760186e9c6595bec
calvinongjr <calvin.ong.2024@computing.smu.edu.sg>
2025-10-06 15:56:25 +0800

    fix swagger ui

--- backend/app/src/test/java/persistence/DatabaseConnectionTest.java
+++ backend/app/src/test/java/persistence/DatabaseConnectionTest.java
@@ -2,6 +2,7 @@ package persistence;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import web.AppApplication;
@@ -9,18 +10,18 @@ import web.AppApplication;
 @SpringBootTest(classes = AppApplication.class)
 public class DatabaseConnectionTest {
     @Autowired
+    @Qualifier("appJdbcTemplate")
     private JdbcTemplate jdbcTemplate;
 
     @Test
     public void testDatabaseConnection() {
         jdbcTemplate.query(
-            "SELECT * FROM Products LIMIT 5",
-            (rs, rowNum) -> String.format(
-                "Product ID: %d, HS Code: %s, Name: %s",
-                rs.getInt("codeUnique"),
-                rs.getString("hs_code"),
-                rs.getString("name")
-            )
-        ).forEach(System.out::println);
+                "SELECT * FROM Products LIMIT 5",
+                (rs, rowNum) -> String.format(
+                        "Product ID: %d, HS Code: %s, Name: %s",
+                        rs.getInt("codeUnique"),
+                        rs.getString("hs_code"),
+                        rs.getString("name")))
+                .forEach(System.out::println);
     }
 }
