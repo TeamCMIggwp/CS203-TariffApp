9b959b088b5db3b9c7ffbfaaee898740ba1896d6
calvinongjr <calvin.ong.2024@computing.smu.edu.sg>
2025-10-19 15:12:29 +0800

    fix #3

--- backend/app/src/main/java/exchangerate/ExchangeRateService.java
+++ backend/app/src/main/java/exchangerate/ExchangeRateService.java
@@ -1,8 +1,5 @@
 package exchangerate;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface ExchangeRateService {
     ExchangeRateResponse getExchangeRates(String base);
 }
