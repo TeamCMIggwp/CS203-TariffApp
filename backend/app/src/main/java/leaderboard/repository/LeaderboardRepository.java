dbdb9fd80f2f2749bcc4948e8dd83aafdc104e84
WaihoisGeetHub <hoi.wai.2024@smu.edu.sg>
2025-11-11 10:40:44 +0800

    bush

--- backend/app/src/main/java/leaderboard/repository/LeaderboardRepository.java
+++ backend/app/src/main/java/leaderboard/repository/LeaderboardRepository.java
@@ -2,6 +2,7 @@ package leaderboard.repository;
 
 import leaderboard.entity.LeaderboardEntryEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -13,6 +14,7 @@ import java.util.Optional;
 public class LeaderboardRepository {
 
     @Autowired
+    @Qualifier("appJdbcTemplate")
     private JdbcTemplate jdbcTemplate;
 
     private final RowMapper<LeaderboardEntryEntity> rowMapper = (rs, rowNum) ->
