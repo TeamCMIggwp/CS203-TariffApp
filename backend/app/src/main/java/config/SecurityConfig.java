1fc4c0df96a0a580278672df067f303d9c65f0f8
calvinongjr <calvin.ong.2024@computing.smu.edu.sg>
2025-11-11 10:50:01 +0800

    allow me

--- backend/app/src/main/java/config/SecurityConfig.java
+++ backend/app/src/main/java/config/SecurityConfig.java
@@ -86,6 +86,10 @@ public class SecurityConfig {
 				.requestMatchers(HttpMethod.POST, "/api/v1/admin/news/rates").hasRole("ADMIN")
 				.requestMatchers(HttpMethod.GET, "/api/v1/admin/news/rates/**").permitAll()
 
+				.requestMatchers(HttpMethod.GET, "/leaderboard").permitAll()
+				.requestMatchers(HttpMethod.PUT, "/leaderboard").permitAll()
+
+
 				// Everything else requires authentication
 				.anyRequest().authenticated()
 			)
