c9408092dc78c6de40e282a145f746bd9d008e49
calvinongjr <calvin.ong.2024@computing.smu.edu.sg>
2025-11-15 01:27:59 +0800

    removed static RestClient

--- backend/app/src/main/java/web/HelloController.java
+++ backend/app/src/main/java/web/HelloController.java
@@ -1,14 +1,10 @@
 package web;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.media.*;
+import io.swagger.v3.oas.annotations.responses.*;
+import io.swagger.v3.oas.annotations.tags.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Tag(name = "Web", description = "Endpoint to verify running SpringBoot")
